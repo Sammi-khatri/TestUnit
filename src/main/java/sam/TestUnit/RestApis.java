634c4214277a187ce80cb671b0b68d23560cd6a8
sammi khatri <sammikhatri2414@gmail.com>
2021-05-18 13:05:21 +0530

    one test

---
+++ src/main/java/sam/TestUnit/RestApis.java
@@ -0,0 +1,22 @@
+package sam.TestUnit;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class RestApis {
+	@GetMapping("/products")
+	public String test() {
+		System.out.println("test APi called");
+		return "Success";
+	}
+
+	@GetMapping("/")
+	private String test1() {
+		System.out.println("test APi called");
+		return "Success";
+	}
+}
+	
---
+++ src/test/java/sam/TestUnit/TestUnitApplicationTests.java
@@ -0,0 +1,47 @@
+package sam.TestUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest
+class TestUnitApplicationTests {
+
+	@Autowired
+	WebApplicationContext webApplicationContext;
+
+	@Test
+	@DisplayName("testInt")
+	void testInt() {
+		int a = 10;
+		Assert.assertEquals(a, a);
+	}
+
+	@Test
+	public void getProductsList() {
+		try {
+			System.out.println("test Called");
+			String uri = "/products";
+			MockMvc mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+			MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri).accept(MediaType.APPLICATION_JSON_VALUE))
+					.andReturn();
+
+			int status = mvcResult.getResponse().getStatus();
+			assertEquals(200, status);
+//	      String content = mvcResult.getResponse().getContentAsString();
+		} catch (Exception e) {
+			System.out.println("exception : " + e.getLocalizedMessage());
+		}
+	}
+
+}
