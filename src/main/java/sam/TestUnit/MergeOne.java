f799386b7fb8f3de569c2e7a9a39b4a7af1cd3e5
sammi khatri <sammikhatri2414@gmail.com>
2021-05-21 18:49:23 +0530

    test

--- src/main/java/sam/TestUnit/MergeOne.java
+++
@@ -1,9 +0,0 @@
-package sam.TestUnit;
-
-import ch.qos.logback.core.net.SyslogOutputStream;
-
-public class MergeOne {
-    public void changeInBrach(){
-        System.out.println("cbhdbvhj");
-    }
-}
